1b5d3bff2d6ee86028bcec6f96cf222d5a9e50da
Camila Braúna <125037138+Cam1ss@users.noreply.github.com>
2025-01-09 15:32:24 -0300

    Delete src directory

--- src/db/config.js
+++
@@ -1,4 +0,0 @@
-const config = {
-    mongoURI: 'mongodb+srv://andersonmaia63:c0bral1ng0@sunrisecluster.gflir.mongodb.net/Festivale?retryWrites=true&w=majority&appName=Festivale'
-};
-module.exports = config;
